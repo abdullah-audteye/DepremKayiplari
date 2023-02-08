f4aa9996a958fd12f9927072de88eb6e52518b01
demirbasharun <harun@audteye.com>
2023-02-08 18:31:39 +0300

    Models and New Status Page added

--- appname/admin.py
+++ appname/admin.py
@@ -2,10 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Tag, IhbarUser, KayipUser, Ihbar,TagArabic,KayipStatus,Countries
+from .models import Tag, IhbarUser, KayipUser, Ihbar,KayipStatus,Countries
 
 admin.site.register(Tag)
-admin.site.register(TagArabic)
 admin.site.register(IhbarUser)
 admin.site.register(KayipUser)
 admin.site.register(Ihbar)
