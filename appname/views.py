c786aaa9793e44d170f49d2cbb9b5ab128443769
Mehmet Demir <mehmet@karson.com.tr>
2023-02-08 15:04:28 +0300

    filter by status

--- appname/views.py
+++ appname/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render, redirect
-from .forms import KayipUserForm,IhbarUserForm
-from .models import Ihbar,KayipUser,Tag,TagArabic,Countries,KayipStatus
-from django.db import  transaction
+from .forms import KayipUserForm, IhbarUserForm
+from .models import Ihbar, KayipUser, Tag, TagArabic, Countries, KayipStatus
+from django.db import transaction
 from django.http import JsonResponse
 from django.http import QueryDict
-from .serializers import KayipUserSerializer,IhbarSerializer
+from .serializers import KayipUserSerializer, IhbarSerializer, KayipStatusSerializer
 from rest_framework.generics import ListAPIView
-
-
 
 
 def IhbarView(request):
@@ -20,7 +18,7 @@ def IhbarView(request):
     if request.method == "POST":
         kayip_user_data = (request.POST.getlist('data[]'))
         ihbarci_data = QueryDict(request.POST.get('ihbarci_data'))
-        ihbaruserform=IhbarUserForm(ihbarci_data)
+        ihbaruserform = IhbarUserForm(ihbarci_data)
 
         if ihbaruserform.is_valid():
             cordinate_x = ihbarci_data.get('cordinate_x')
@@ -28,38 +26,41 @@ def IhbarView(request):
 
             try:
                 with transaction.atomic():
-                    ihbaruserform_instance=ihbaruserform.save()
-                    
+                    ihbaruserform_instance = ihbaruserform.save()
+
                     saved_records = []
                     for kayip_user in kayip_user_data:
                         kayip_user = QueryDict(kayip_user).copy()
-                        kayip_user['cordinate_x']=cordinate_x
-                        kayip_user['cordinate_y']=cordinate_y
+                        kayip_user['cordinate_x'] = cordinate_x
+                        kayip_user['cordinate_y'] = cordinate_y
 
                         kayip_user_check = KayipUserForm(kayip_user)
-                        if(kayip_user_check.is_valid()):
+                        if (kayip_user_check.is_valid()):
                             kayip_user_instance = kayip_user_check.save()
-                                
-                            saved_records.append(kayip_user_instance.id)
 
+                            saved_records.append(kayip_user_instance.id)
 
                     ihbar_instance = Ihbar.objects.create()
                     ihbar_instance.ihbar_user = ihbaruserform_instance
                     ihbar_instance.kayip_user.add(*saved_records)
 
                     ihbar_instance.save()
-                    return JsonResponse({'status':True,'message':"success"}, status=200)
-                
+                    return JsonResponse({'status': True, 'message': "success"}, status=200)
+
 
             except Exception as err:
-                print(err,'errrr')
-            
+                print(err, 'errrr')
 
-
-    return render(request,"ihbar.html",{"kayipuserform":kayipuserform,"ihbaruserform":ihbaruserform,"tags":tags,"countries":countries,"kayip_status":kayipstatus})
+    return render(request, "ihbar.html",
+                  {"kayipuserform": kayipuserform, "ihbaruserform": ihbaruserform, "tags": tags, "countries": countries,
+                   "kayip_status": kayipstatus})
 
 
 class KayipUserListView(ListAPIView):
     queryset = Ihbar.objects.order_by('-id')
     serializer_class = IhbarSerializer
 
+
+class KayipStatusListView(ListAPIView):
+    queryset = KayipStatus.objects.all()
+    serializer_class = KayipStatusSerializer
