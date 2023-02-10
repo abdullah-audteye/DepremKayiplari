b82cbc5950b8a802eeb72169e0e7d34f3c97ae55
demirbasharun <harun@audteye.com>
2023-02-10 18:10:09 +0300

    remove iframe pages

--- projectname/urls.py
+++ projectname/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from django.contrib import admin
-from appname.views import IhbarView,KayipUserListView,KayipUserFilterUser,KayipStatusListView,ChangeKayipStatus,IframeForm,GeneralFormDataView,IframeDashboard
+from appname.views import IhbarView,KayipUserListView,KayipUserFilterUser,KayipStatusListView,ChangeKayipStatus,GeneralFormDataView
 
 
 urlpatterns = [
@@ -10,12 +10,8 @@ urlpatterns = [
     path('genelform',GeneralFormDataView,name="genelform"),
 
 
-    path('form',IframeForm,name="iframe_form"),
-    # path('dashboard',IframeDashboard,name="iframe_dashboard"),
-
     path('api/kayiplar',KayipUserListView.as_view(),name="kayiplarview_api"),
     path('api/kayiplar/filter',KayipUserFilterUser.as_view(),name="kayiplarview_api_filter"),
-
     path('api/kayipstatus',KayipStatusListView.as_view(),name="kayip_status_api")
 
 ]
