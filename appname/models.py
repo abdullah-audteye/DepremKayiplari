a79419d2ed05cc036ad911b25204dab374c5d6ff
dev <basheer@audteye.com>
2023-02-11 22:17:30 +0300

    add created_at to KayipUser model

--- appname/models.py
+++ appname/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from datetime import datetime
-
+from django.utils import timezone
 
 
 class Tag(models.Model):
@@ -70,6 +70,8 @@ class KayipUser(models.Model):
     kayip_status = models.ForeignKey(KayipStatus,on_delete=models.CASCADE,null=True,blank=True,related_name="kayiplar")
     gender = models.CharField(max_length=1,choices=GENDER_CHOICES,null=True,blank=True)
     age = models.IntegerField(null=True,blank=True,default=0)
+    created_at = models.DateTimeField(blank=True,null=True,default=timezone.now)
+
 
     def __str__(self):
         return self.kayip_first_name + " - "+self.kayip_last_name
