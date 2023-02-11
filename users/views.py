ec88c1e627a504d90a63f7f4fe291670ea60aea5
yusufziyacivan <yusufziyacivan@gmail.com>
2023-02-11 13:34:01 +0300

    Adds login page, authenticate the user when fill the form is correct

--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from users.models import User
 
-# Create your views here.
+def login_view(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        if user is not None:
+            # Login the user
+            login(request, user)
+            return redirect('item_list')
+        else:
+            return render(request, 'login.html', {'message': 'Invalid credentials'})
