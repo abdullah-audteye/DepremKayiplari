fbc7906a97ada388af8749cc656c3b97d94f0477
demirbasharun <harun@audteye.com>
2023-02-15 18:04:17 +0300

    cors added

--- projectname/settings.py
+++ projectname/settings.py
@@ -40,6 +40,7 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 X_FRAME_OPTIONS = 'ALLOW-FROM http://*.localhost/(.*)'
+CORS_ALLOW_ALL_ORIGINS=True
 
 
 # Application definition
@@ -53,12 +54,16 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'appname',
+    "corsheaders",
+
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
